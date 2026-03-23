994fbd2cf4f606848d907cc061d5a74ab1b05e32
pcc <pablo.casado92@gmail.com>
2026-03-23 18:14:09 +0100

    Creación del javadoc

---
+++ figuras/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"figuras","l":"Cuadrado"},{"p":"figuras","l":"Figura"},{"p":"figuras","l":"PruebaFigura"},{"p":"figuras","l":"Punto"},{"p":"figuras","l":"Rectangulo"},{"p":"figuras","l":"Triangulo"}];updateSearchResults();
